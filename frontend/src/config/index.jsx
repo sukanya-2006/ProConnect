5d09adb845caa65a2761138560abf3f97eabb867
sukanya-2006 <sukanyabhowmick094@gmail.com>
2026-06-10 00:36:03 +0530

    changed the base url to deploy

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:9090";
+export const BASE_URL = "https://proconnect-6tjm.onrender.com/";
 
 export const clientServer = axios.create({
     baseURL: BASE_URL,
