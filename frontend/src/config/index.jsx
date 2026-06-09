2a7d8c431f8459786aa75111a00470e358bd79d9
sukanya-2006 <sukanyabhowmick094@gmail.com>
2026-06-10 03:58:24 +0530

    fix: remove trailing slash from BASE_URL, fix post media path

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 
-export const BASE_URL = "https://proconnect-6tjm.onrender.com/";
+export const BASE_URL = "https://proconnect-6tjm.onrender.com";
 
 export const clientServer = axios.create({
     baseURL: BASE_URL,
